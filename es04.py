2508db934c0cf516a0c7eb0361c38bdd9cf17526
Riccardo <r.cuccu@outlook.it>
2020-05-05 18:22:13 +0200

    Exercise

--- es04.py
+++ es04.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python
+
+s = "abcdefgabc"
+D = {}
+
+for char in s:
+	if char not in D.keys():
+		D[char] = 1
+	else:
+		D[char] += 1
+
+for (key, value) in D.items():
+	print(key + ',' + str(value))
--- es05.py
+++ es05.py
@@ -1,1 +1,28 @@
 #!/usr/bin/env python
+
+n = input("Enter a number and i will tell you if it is a prime number: ")
+
+try:
+	n = int(n)
+except:
+	print("This is not a number!")
+else:
+
+	if n < 0:
+		print("Number entered less than zero.")
+	elif n == 0:
+		print("Number entered equal to zero.")
+	else:
+	
+		div = 2
+		count = 0
+	
+		while div <= n/2 and count==0:
+			if n%div==0:
+				count+=1
+			div+=1
+	
+		if count==0:
+			print("%d is a prime number! :D" % n)
+		else:
+			print("%d in not a prime number! :(" % n)
