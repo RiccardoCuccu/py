524fd4a1165aba1e9e61708ce33dd4d505b00a51
Riccardo <r.cuccu@outlook.it>
2020-06-29 09:51:28 +0200

    Exercise

--- es05.py
+++ es05.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python
 
-n = input("Enter a number and i will tell you if it is a prime number: ")
+n = input("Enter a number and I will tell you if it is a prime number: ")
 
 try:
 	n = int(n)
 except:
 	print("This is not a number!")
 else:
-
 	if n < 0:
 		print("Number entered less than zero.")
 	elif n == 0:
 		print("Number entered equal to zero.")
 	else:
 	
-		div = 2
-		count = 0
+		div = 2							# set the first divisor
+		flag = 0						# set the flag to zero
 	
-		while div <= n/2 and count==0:
-			if n%div==0:
-				count+=1
-			div+=1
+		while div <= n/2 and flag==0:				# as long as the divisor is smaller than half the number entered and flag = 0
+			if n%div==0:					# if a divisor has been found that gives zero remainder
+				flag+=1					# set the flag to one (the number entered is not a prime number)
+			div+=1						# increase the divisor
 	
-		if count==0:
+		if flag==0:
 			print("%d is a prime number! :D" % n)
 		else:
 			print("%d in not a prime number! :(" % n)
