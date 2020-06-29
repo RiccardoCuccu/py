a45e5d60c9ecf1233ff57bb9c169213b26443912
Riccardo <r.cuccu@outlook.it>
2020-06-29 09:55:30 +0200

    Exercise

--- es05.py
+++ es05.py
@@ -16,8 +16,8 @@ else:
 		div = 2							# set the first divisor
 		flag = 0						# set the flag to zero
 	
-		while div <= n/2 and flag==0:				# as long as the divisor is smaller than half the number entered and flag = 0
-			if n%div==0:					# if a divisor has been found that gives zero remainder
+		while div <= n/2 and flag==0:				# as long as the divisor is smaller than half the number entered and flag is zero
+			if n%div==0:					# if a divisor that gives zero remainder has been found
 				flag+=1					# set the flag to one (the number entered is not a prime number)
 			div+=1						# increase the divisor
 	
